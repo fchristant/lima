5f57dd847f7146c26a70998e9477c6de88b3b588
Ferdy <ferdy.christant@gmail.com>
2023-08-11 20:44:44 +0200

    cleanup and basic page layout

---
+++ src/app/home/footer.tsx
@@ -0,0 +1,9 @@
+import './footer.css'
+
+export default function Footer() {
+  return (
+      <footer className="layout-footer">
+         <small>footer</small>
+      </footer>
+  )
+}
---
+++ src/app/home/header.tsx
@@ -0,0 +1,9 @@
+import './header.css'
+
+export default function Header() {
+  return (
+      <header className='layout-header'>
+         <h1>Philips Hue Web App</h1>
+      </header>
+  )
+}
