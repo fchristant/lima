d9fb8f1f944246c7011510804b591bacb84bc484
Ferdy <ferdy.christant@gmail.com>
2023-08-18 17:06:27 +0200

    made api protocol configurable

--- src/app/home/footer.tsx
+++ src/app/home/footer.tsx
@@ -3,7 +3,7 @@ import './footer.css'
 export default function Footer() {
   return (
       <footer className="layout-footer">
-         <p><small>{process.env.NEXT_PUBLIC_HUE_USERNAME}@{process.env.NEXT_PUBLIC_HUE_IP}</small></p>
+         <p><small>{process.env.NEXT_PUBLIC_HUE_USERNAME}@{process.env.NEXT_PUBLIC_HUE_PROTOCOL}://{process.env.NEXT_PUBLIC_HUE_IP}</small></p>
       </footer>
   )
 }
