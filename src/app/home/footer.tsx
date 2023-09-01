74d6f1a5fa8321a130ed5ceab58fe787990fff3b
Ferdy <ferdy.christant@gmail.com>
2023-09-01 15:05:37 +0200

    added credits to footer

--- src/app/home/footer.tsx
+++ src/app/home/footer.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
   return (
       <footer className="layout-footer">
          <p><small>{process.env.NEXT_PUBLIC_HUE_API_USERNAME}@{process.env.NEXT_PUBLIC_HUE_API_ADDRESS}</small></p>
+         <p>A project by <a href="https://www.ferdychristant.com" target='new'>Ferdy Christant</a></p>
       </footer>
   )
 }
