807657dbbd6ac3b1fc82a08ebe470df2ac05be4f
Ferdy <ferdy.christant@gmail.com>
2023-09-16 22:08:46 +0200

    added scene selection

--- src/app/components/Scene/Scene.tsx
+++ src/app/components/Scene/Scene.tsx
@@ -6,7 +6,32 @@ interface SceneProps {
 }
 
 export default function Scene({ scene }: SceneProps) {
-  const { num, name, type } = scene;
+  const { num, name, type, group } = scene;
+  const ENDPOINT = `${process.env.NEXT_PUBLIC_HUE_API_ADDRESS}/api/${process.env.NEXT_PUBLIC_HUE_API_USERNAME}/groups/${group}/action`;
 
-  return <button className="scene">{name}</button>;
+  async function recallScene(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    const bodyData = { scene: num };
+    try {
+      const response = await fetch(ENDPOINT, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(bodyData),
+      });
+
+      const data = await response.json();
+
+      if (data[0]?.error) {
+        console.error("Error:", data[0].error.description);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  return (
+    <button className="scene" onClick={recallScene}>
+      {name}
+    </button>
+  );
 }
