1b1a45ac9ec97bf960dfb9c5fff97ae26d4e8014
Ferdy <ferdy.christant@gmail.com>
2023-10-01 14:49:28 +0200

    added github link to footer

--- src/app/components/Page/Footer.tsx
+++ src/app/components/Page/Footer.tsx
@@ -4,9 +4,14 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p>
-        A project by{" "}
+        An open source project by{" "}
         <a href="https://www.ferdychristant.com" target="new">
           Ferdy Christant
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/fchristant/lima" target="new">
+          Github
         </a>
       </p>
     </footer>
